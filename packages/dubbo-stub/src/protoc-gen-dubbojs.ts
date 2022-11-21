7a6fc4f94473251acf65b77c6246deee8d4f84b5
杨晓东 <coder.yang2010@gmail.com>
2022-11-21 17:46:45 +0800

    add: 添加idl文件示例

--- packages/dubbo-stub/src/protoc-gen-dubbojs.ts
+++ packages/dubbo-stub/src/protoc-gen-dubbojs.ts
@@ -32,57 +32,6 @@ async function main() {
   ).bind(process.stdout)
   await write(Buffer.from(buffer))
 }
-// import {readToBuffer} from "./util";
-// import { prefixDisableLinter, protoFilesToGenerate, readToBuffer } from "./utils";
-// import { generateFile, makeUtils } from "./main";
-// import { createTypeMap } from "./types";
-// import { Context } from "./context";
-// import { getTsPoetOpts, optionsFromParameter } from "./options";
-// import { generateTypeRegistry } from "./generate-type-registry";
-// console.log('hello ');
-// // this would be the plugin called by the protoc compiler
-// async function main() {
-//   const stdin = await readToBuffer(process.stdin);
-//   // const json = JSON.parse(stdin.toString());
-//   // const request = CodeGeneratorRequest.fromObject(json);
-//   console.log(stdin.toString());
-//   // const request = CodeGeneratorRequest.decode(stdin);
-//   //
-//   // const options = optionsFromParameter(request.parameter);
-//   // const typeMap = createTypeMap(request, options);
-//   // const utils = makeUtils(options);
-//   // const ctx: Context = { typeMap, options, utils };
-//   //
-//   // const filesToGenerate = options.emitImportedFiles ? request.protoFile : protoFilesToGenerate(request);
-//   // const files = await Promise.all(
-//   //   filesToGenerate
-//   //     .filter((file) => !options.M[file.name])
-//   //     .map(async (file) => {
-//   //       const [path, code] = generateFile(ctx, file);
-//   //       const content = code.toString({ ...getTsPoetOpts(options), path });
-//   //       return { name: path, content };
-//   //     })
-//   // );
-//   //
-//   // if (options.outputTypeRegistry) {
-//   //   const utils = makeUtils(options);
-//   //   const ctx: Context = { options, typeMap, utils };
-//   //
-//   //   const path = "typeRegistry.ts";
-//   //   const code = generateTypeRegistry(ctx);
-//   //
-//   //   const content = code.toString({ ...getTsPoetOpts(options), path });
-//   //   files.push({ name: path, content });
-//   // }
-//   //
-//   // const response = CodeGeneratorResponse.fromPartial({
-//   //   file: files,
-//   //   supportedFeatures: CodeGeneratorResponse_Feature.FEATURE_PROTO3_OPTIONAL,
-//   // });
-//   // const buffer = CodeGeneratorResponse.encode(response).finish();
-//   // const write = promisify(process.stdout.write as (buffer: Buffer) => boolean).bind(process.stdout);
-//   // await write(Buffer.from(buffer));
-// }
 
 main()
   .then(() => {
